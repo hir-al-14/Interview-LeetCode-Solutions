67b7a76bdbe131df22839619eccb9fe862f395f1
Hiral Arora <hiral.arora1406@gmail.com>
2026-06-21 22:21:50 -0700

    swiched to just measure max distance reachable from start index 0

--- jump_game.py
+++ jump_game.py
@@ -1,15 +1,10 @@
 class Solution:
-    def helper(self, nums, c, total):
-        if c >= total:
-            return True
-        for j in range(0, nums[c], 1):
-            a = nums[j]
-            if self.helper(nums, c + j, total):
-                return True
-        return False
     def canJump(self, nums: List[int]) -> bool:
+        max_dist = 0
         n = len(nums)
-        out = False
-        cur = 0
-        out = self.helper(nums, cur, n-1)
-        return out
+        for i in range(nums):
+            jump_value = nums[i]
+            if i > max_dist:
+                return False
+            max_dist = max(max_dist, i + jump_value)
+        return max_dist
