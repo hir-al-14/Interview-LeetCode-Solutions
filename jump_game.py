a1e97d87ffbff30f8e4015a0d364ec3217d42ad3
Hiral Arora <hiral.arora1406@gmail.com>
2026-06-21 20:52:52 -0700

    first thought

--- jump_game.py
+++ jump_game.py
@@ -1,3 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-      
+        n = len(nums)
+        for i in range(nums):
+            # Base Case
+            if nums[i] == n:
+                return True
+            for j in range(0, i, 1):
+                a = nums[i]
+                if (a + nums[a]) == n:
+                    return True
+                else:
+                    # recursive call
