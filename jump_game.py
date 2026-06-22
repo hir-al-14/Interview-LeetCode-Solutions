f1e54d10336b04b4173341bf4d5731fc43b485ef
Hiral Arora <hiral.arora1406@gmail.com>
2026-06-21 22:30:20 -0700

    added comments

--- jump_game.py
+++ jump_game.py
@@ -1,12 +1,17 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        # max distance reachable at start
         max_dist = 0
         n = len(nums)
+        # go through each index and update the max duisatnce reachable from start to that index
         for i in range(n):
+            # max jump length from that index
             jump_value = nums[i]
-            if i > max_dist:
+            if i > max_dist: # we can't go further than max distance
                 return False
+            # tracking furthest distance reachable from all positions visited
             max_dist = max(max_dist, i + jump_value)
+        # check and return
         if max_dist >= n - 1:
             return True
         else: return False
