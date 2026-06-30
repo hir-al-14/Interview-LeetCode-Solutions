4b0515509171f653d5623f6a7c52f7024056ada9
Hiral Arora <hiral.arora1406@gmail.com>
2026-06-30 10:50:05 -0700

    finding where zero exists and mark that row and column

--- matrix_zero.py
+++ matrix_zero.py
@@ -1,6 +1,18 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
+        a = len(matrix)
+        b = len(matrix[0])
+
+        rows_with_0 = [0] * a
+        cols_with_0 = [0] * b
+
+        // find row, col containing a zero
+        for i in range(a):
+            for j in range(b):
+                if matrix[i][j] == 0:
+                    rows_with_0[i] = 1
+                    cols_with_0[j] = 1
+
     }
 };
