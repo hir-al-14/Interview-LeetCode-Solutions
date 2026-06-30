05f2389609adfc2124c046ad240ed8e8578fe5d9
Hiral Arora <hiral.arora1406@gmail.com>
2026-06-30 11:14:35 -0700

    changed to instead track rows and columns that should have 0 within the matrix only instead of seperate lists

--- matrix_zero.py
+++ matrix_zero.py
@@ -8,6 +8,37 @@ class Solution:
 
         rows_with_0 = [0] * a
         cols_with_0 = [0] * b
+       
+       # i wanted to make it O(1) so i tried to use matrix itself to track which rows and columns should have 0
+       # instead of using seperate lists to track which rows and col should have 0
+
+        # find row, col containing a zero
+        for i in range(a):
+            for j in range(b):
+                if matrix[i][j] == 0:
+                    # makes the first element of that row 0
+                    matrix[i][0] = 0
+                    # makes the first element of that col 0
+                    matrix[0][j] = 0
+
+        # set the rows and columns to zero
+        for i in range(a):
+            for j in range(b):
+                # if first element of that row or col is a 0 that indicates that that all others elements there should also be 0's
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+
+"""
+i got O(m*n) time complexity for this old solution on leetcode
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        a = len(matrix)
+        b = len(matrix[0])
+
+        rows_with_0 = [0] * a
+        cols_with_0 = [0] * b
 
         # find row, col containing a zero
         for i in range(a):
@@ -20,4 +51,4 @@ class Solution:
             for j in range(b):
                 if rows_with_0[i] or cols_with_0[j]:
                     matrix[i][j] = 0
-             
+"""
