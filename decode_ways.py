d1cf1ff2a7b20bce07453f0a7a24f9f295827fa7
Hiral Arora <hiral.arora1406@gmail.com>
2026-07-01 06:04:08 -0700

    made an incremented l;ist that keep track of decode ways at each i value

--- decode_ways.py
+++ decode_ways.py
@@ -1,30 +1,29 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        count = 0
-        is_valid = True
 
         # Base case- as 0 is not a valid decosde and 01 or any other num is also not valid
         if s[0] == '0':
             return 0
         
-        # everytime we need to keep track of the previous and previous-1 character
+        # decode_ways- number of ways to decode the first i characters of the string
         decode_ways = [0]*(len(s) + 1)
         decode_ways[0] = 1
-        decode_ways[1] = 1
 
         for i in range(2, len(s)+1):
+            if decode_ways[i] == 0:
+                continue
+
             # j is the length - either 1 or 2 length can be decoded
             for j in [1,2]:
+                one = s[i:i+j]
                 if i + j > len(s):
                     continue
 
                 part = s[i:i+j]
 
                 if (j == 1) and (part!='0'):
-                    is_valid = True
+                    decode_ways[i+j] += decode_ways[i]
                 elif (j == 2) and (10 <= int(part) <= 26):
-                    is_valid = True
+                    decode_ways[i+j] += decode_ways[i]
 
-            if is_valid:
-                count += 1
-        return count
+        return decode_ways[len(s)] # Total ways to decode the entire string
