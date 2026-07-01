6c8e543e20cfcef7b6db73bcbc8b59f494a6d8f7
Hiral Arora <hiral.arora1406@gmail.com>
2026-07-01 05:58:24 -0700

    added a decode ways array that has the num of ways an ith character can be decoded

--- decode_ways.py
+++ decode_ways.py
@@ -6,8 +6,13 @@ class Solution:
         # Base case- as 0 is not a valid decosde and 01 or any other num is also not valid
         if s[0] == '0':
             return 0
+        
+        # everytime we need to keep track of the previous and previous-1 character
+        decode_ways = [0]*(len(s) + 1)
+        decode_ways[0] = 1
+        decode_ways[1] = 1
 
-        for i in range(len(s)):
+        for i in range(2, len(s)+1):
             # j is the length - either 1 or 2 length can be decoded
             for j in [1,2]:
                 if i + j > len(s):
