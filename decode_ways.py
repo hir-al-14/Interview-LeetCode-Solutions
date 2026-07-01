c4d941ec21a5f4b7fa2a7f16208cdd5f392951c1
Hiral Arora <hiral.arora1406@gmail.com>
2026-07-01 05:51:22 -0700

    made is_valid condition fixes

--- decode_ways.py
+++ decode_ways.py
@@ -6,15 +6,20 @@ class Solution:
         # Base case- as 0 is not a valid decosde and 01 or any other num is also not valid
         if s[0] == '0':
             return 0
+
         for i in range(len(s)):
-            for j in [0,1]:
-                if i + j < len(s):
-                    if (s[i] == '0'):
-                        is_valid = True
-                    elif (s[i+j] == '0'):
-                        is_valid = True
-                    else:
-                        is_valid = True
+            # j is the length - either 1 or 2 length can be decoded
+            for j in [1,2]:
+                if i + j > len(s):
+                    continue
+
+                part = s[i:i+j]
+
+                if (j == 1) and (part!='0'):
+                    is_valid = True
+                elif (j == 2) and (10 <= int(part) <= 26):
+                    is_valid = True
+
             if is_valid:
                 count += 1
         return count
