52065148f3b27d6c9c8e67dd5d30e01103ccda97
Hiral Arora <hiral.arora1406@gmail.com>
2026-07-01 00:22:10 -0700

    initial setup for decode ways

--- matrix_zero.py
+++ matrix_zero.py
@@ -1,8 +1,5 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         a = len(matrix)
         b = len(matrix[0])
 
